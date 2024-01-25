2744a8d11d43477839eac1b9b0f2b4b5d5bb3334
JT Raber <JTRaber@discount-drugmart.com>
2024-01-25 16:27:48 -0500

    not much

--- static/js/util.js
+++ static/js/util.js
@@ -7,3 +7,30 @@ export const randint = (lower, upper) => {
 	const real = randreal(lower, upper);
 	return Math.floor(real);
 }
+
+export const notes = {
+	"C": 261.63,
+	"C#": 277.18,
+	"Db": 277.18,
+	"D": 293.66,
+	"D#": 311.13,
+	"Eb": 311.13,
+	"E": 329.63,
+	"F": 349.23,
+	"F#": 369.99,
+	"Gb": 369.99,
+	"G": 392.00,
+	"G#": 415.30,
+	"Ab": 415.30,
+	"A": 440.00,
+	"A#": 466.16,
+	"Bb": 466.16,
+	"B": 493.88,
+}
+
+export const getNote = (note, octave) => {
+	const baseFrequency = notes[note];
+	const octaveShift = octave - 4;
+	const multiplier = Math.pow(2, octaveShift);
+	return baseFrequency * multiplier;
+}
